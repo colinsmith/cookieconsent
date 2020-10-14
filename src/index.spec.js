f6e5d46ad60befe84a567b13838cd30f47830762
Marek Rozmus <marek.rozmus@sandstream.pl>
2020-10-14 13:03:10 +0200

    Update tests to work with latest changes

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,7 +1,7 @@
 "use strict"
 
 const CookieConsent = require( "./models/CookieConsent" ).default
-const CC = require( "./" )
+const { CookieConsent: CC } = require( "./" )
 
 describe( "Plugin", () => {
   test( "instantiates without errors", () => {
