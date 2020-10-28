f4e28062a9e51b566c6cb9dded15f5d49ec2f073
Marek Rozmus <marek.rozmus@sandstream.pl>
2020-10-28 14:25:42 +0100

    Add same site cookies support

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -11,20 +11,21 @@ export const getCookie = name => {
         .shift()
 }
 
-export const setCookie = function ( name, value, expiryDays, domain, path, secure = true ) {
+export const setCookie = function ( name, value, expiryDays, domain, path, secure, sameSite ) {
   const exdate = new Date()
   exdate.setHours(exdate.getHours() + ((typeof expiryDays !== "number"  ? 365 : expiryDays ) * 24))
   document.cookie = name + '=' + value +
                     ';expires=' + exdate.toUTCString() +
                     ';path=' + (path || '/') +
                     ( domain ? ';domain=' + domain : '' ) +
-                    ( secure ? ';secure' : '' )
+                    ( secure ? ';secure' : '' ) +
+                    ( sameSite ? ';SameSite=' + sameSite : '' );
 }
 
 export const deleteCookie = (name, attributes) => {
   if (getCookie(name)) {
-    const { domain, path, secure } = attributes;
+    const { domain, path, secure, sameSite } = attributes;
 
-    setCookie(name, '', -1, domain, path, secure);
+    setCookie(name, '', -1, domain, path, secure, sameSite);
   }
 }
